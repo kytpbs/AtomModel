214a1100096f68af07a11d3b2019df0f0db6c180
Kaya <kytpbs@gmail.com>
2023-09-21 09:17:04 +0300

    rename strip variable and names

--- AtomModel.ino
+++ AtomModel.ino
@@ -2,16 +2,16 @@
 #include "Constants.h"
 #include "strips.h"
 
-Adafruit_NeoPixel lowerStrip(UPNUMPIXELS, UPDATA, NEO_GRB + NEO_KHZ800); // Create the lower strip object
-Adafruit_NeoPixel upperStrip(DOWNNUMPIXELS, DOWNDATA, NEO_GRB + NEO_KHZ800); // Create the upper strip object
+Adafruit_NeoPixel innerStrip(UPNUMPIXELS, UPDATA, NEO_GRB + NEO_KHZ800); // Create the inner strip object
+Adafruit_NeoPixel outerStrip(DOWNNUMPIXELS, DOWNDATA, NEO_GRB + NEO_KHZ800); // Create the outer strip object
 
-int lowerStripPixelIndex = 0; // the index of the first electron / pixel in the lower strip
-int upperStripPixelIndex = 0; // the index of the first electron / pixel in the upper strip
+int innerStripPixelIndex = 0; // the index of the first electron / pixel in the inner strip
+int outerStripPixelIndex = 0; // the index of the first electron / pixel in the outer strip
 int togetherAmount = 0;
 
 // I will use these variables when changing the amount of pixels that are lit up on each.
-int lowerPixelAmount = PIXELAMOUNT;
-int upperPixelAmount = PIXELAMOUNT;
+int innerPixelAmount = PIXELAMOUNT;
+int outerPixelAmount = PIXELAMOUNT;
 
 // it is set to true because, we start with the two pixels together
 bool wasTogether = true; // This is used to check if the two pixels were together in the last iteration, we use this so that we don't increase the togetherAmount every iteration they are together
@@ -29,23 +29,23 @@ void setup() {
 }
 
 void setupStrips() {
-  // Setup lower strip
-  Serial.println("Setting up lower strip...");
-  lowerStrip.begin(); // This initializes the strip
-  lowerStrip.setBrightness(BRIGHTNESS); // Set the brightness of the strip
-  lowerStrip.clear(); // Set all pixel colors to 'off'
-  lowerStrip.show(); // Initialize all pixels to 'off'
+  // Setup inner strip
+  Serial.println("Setting up inner strip...");
+  innerStrip.begin(); // This initializes the strip
+  innerStrip.setBrightness(BRIGHTNESS); // Set the brightness of the strip
+  innerStrip.clear(); // Set all pixel colors to 'off'
+  innerStrip.show(); // Initialize all pixels to 'off'
   
-  // Setup upper strip
-  Serial.println("Setting up upper strip...");
-  upperStrip.begin(); // This initializes the strip
-  upperStrip.setBrightness(BRIGHTNESS); // Set the brightness of the strip
-  upperStrip.clear(); // Set all pixel colors to 'off'
-  upperStrip.show(); // Initialize all pixels to 'off'
+  // Setup outer strip
+  Serial.println("Setting up outer strip...");
+  outerStrip.begin(); // This initializes the strip
+  outerStrip.setBrightness(BRIGHTNESS); // Set the brightness of the strip
+  outerStrip.clear(); // Set all pixel colors to 'off'
+  outerStrip.show(); // Initialize all pixels to 'off'
 }
 
 void loop() {
-  if (lowerStripPixelIndex == upperStripPixelIndex) { // If the two pixels are together
+  if (innerStripPixelIndex == outerStripPixelIndex) { // If the two pixels are together
     if (!wasTogether) {
       wasTogether = true;
       Serial.print("Wasn't together before, increasing together amount, New together Amount");
@@ -74,38 +74,38 @@ void loop() {
  * Moves the red color by one pixel from the back to the front removing the red color from the back
 */
 void moveRedFowards() {
-  moveColorFowardOnce(&lowerStrip, lowerStrip.Color(lowerRED, lowerGREEN, lowerBLUE), lowerStrip.Color(lowerBACKGROUNDRED, lowerBACKGROUNDGREEN, lowerBACKGROUNDBLUE), lowerStripPixelIndex, lowerPixelAmount);
-  moveColorFowardOnce(&upperStrip, upperStrip.Color(UPPERRED, UPPERGREEN, UPPERBLUE), upperStrip.Color(UPPERBACKGROUNDRED, UPPERBACKGROUNDGREEN, UPPERBACKGROUNDBLUE), upperStripPixelIndex, upperPixelAmount);
-  lowerStripPixelIndex++;
-  upperStripPixelIndex++;
-  if (lowerStripPixelIndex >= lowerStrip.numPixels()) {
-    lowerStripPixelIndex = 0;
+  moveColorFowardOnce(&innerStrip, innerStrip.Color(innerRED, innerGREEN, innerBLUE), innerStrip.Color(innerBACKGROUNDRED, innerBACKGROUNDGREEN, innerBACKGROUNDBLUE), innerStripPixelIndex, innerPixelAmount);
+  moveColorFowardOnce(&outerStrip, outerStrip.Color(outerRED, outerGREEN, outerBLUE), outerStrip.Color(outerBACKGROUNDRED, outerBACKGROUNDGREEN, outerBACKGROUNDBLUE), outerStripPixelIndex, outerPixelAmount);
+  innerStripPixelIndex++;
+  outerStripPixelIndex++;
+  if (innerStripPixelIndex >= innerStrip.numPixels()) {
+    innerStripPixelIndex = 0;
   }
-  if (upperStripPixelIndex >= upperStrip.numPixels()) {
-    upperStripPixelIndex = 0;
+  if (outerStripPixelIndex >= outerStrip.numPixels()) {
+    outerStripPixelIndex = 0;
   }
 }
 
 void blinkAll(int delayTime, int times) {
   for (int i=0; i<times; i++) {
-    flushColor(&lowerStrip, lowerStrip.Color(BLINKRED, BLINKGREEN, BLINKBLUE));
-    flushColor(&upperStrip, upperStrip.Color(BLINKRED, BLINKGREEN, BLINKBLUE));
+    flushColor(&innerStrip, innerStrip.Color(BLINKRED, BLINKGREEN, BLINKBLUE));
+    flushColor(&outerStrip, outerStrip.Color(BLINKRED, BLINKGREEN, BLINKBLUE));
     delay(delayTime);
-    lowerStrip.clear();
-    upperStrip.clear();
-    lowerStrip.show();
-    upperStrip.show();
+    innerStrip.clear();
+    outerStrip.clear();
+    innerStrip.show();
+    outerStrip.show();
     delay(delayTime);
   }
 }
 
 void switchPixel() {
-  if (lowerPixelAmount <= upperPixelAmount) {
-    lowerPixelAmount += 1;
-    upperPixelAmount -= 1;
+  if (innerPixelAmount <= outerPixelAmount) {
+    innerPixelAmount += 1;
+    outerPixelAmount -= 1;
   }
   else {
-    lowerPixelAmount -= 1;
-    upperPixelAmount += 1;
+    innerPixelAmount -= 1;
+    outerPixelAmount += 1;
   }
 }
--- Constants.h
+++ Constants.h
@@ -1,13 +1,13 @@
-#define UPNUMPIXELS 11 // The number of LEDs in the upper strip
-#define DOWNNUMPIXELS 22 // The number of LEDs in the lower strip
+#define UPNUMPIXELS 11 // The number of LEDs in the outer strip
+#define DOWNNUMPIXELS 22 // The number of LEDs in the inner strip
 
-#define UPDATA 13 // The pin that the upper strip is connected to
-#define DOWNDATA 12 // The pin that the lower strip is connected to
+#define UPDATA 13 // The pin that the outer strip is connected to
+#define DOWNDATA 12 // The pin that the inner strip is connected to
 
 #define PIXELAMOUNT 3 // The number of pixels that are lit up on each strip (will change on pixel switch)
 
 /*
- * The number of pixels between the two pixels including the pixel that are lit up. Should be devidble by both the upper and lower number of pixels.
+ * The number of pixels between the two pixels including the pixel that are lit up. Should be devidble by both the outer and inner number of pixels.
  * This is because we want the pixels to be evenly spaced out. IF THIS IS NOT TRUE, THE PIXELS WILL NOT BE EVENLY SPACED OUT.
 */
 #define PIXELSPACE 11 
@@ -15,15 +15,15 @@
 
 #define BRIGHTNESS 255 // The brightness of the LEDS should be a value from 0 - 255
 
-//Electron Colors of the lower Stshould be values from (0 - 255):
-#define lowerRED 255
-#define lowerGREEN 0
-#define lowerBLUE 255
+//Electron Colors of the inner Stshould be values from (0 - 255):
+#define innerRED 255
+#define innerGREEN 0
+#define innerBLUE 255
 
-// Electron Colors of the Upper Strip should be values from (0 - 255):
-#define UPPERRED 255
-#define UPPERGREEN 255
-#define UPPERBLUE 0
+// Electron Colors of the outer Strip should be values from (0 - 255):
+#define outerRED 255
+#define outerGREEN 255
+#define outerBLUE 0
 
 #define DELAYTIME 75 // The amount of time between each pixel movement in milliseconds.
 #define TOGETHERAMOUNT 5 // The amount of times the pixels need to be together to switch
@@ -36,10 +36,10 @@
 #define BLINKBLUE 255 // The amount of blue in the blink
 
 //Background Color Constants
-#define lowerBACKGROUNDRED 0 // The amount of red in the background color
-#define lowerBACKGROUNDGREEN 255 // The amount of green in the background color
-#define lowerBACKGROUNDBLUE 0 // The amount of blue in the background color
+#define innerBACKGROUNDRED 0 // The amount of red in the background color
+#define innerBACKGROUNDGREEN 255 // The amount of green in the background color
+#define innerBACKGROUNDBLUE 0 // The amount of blue in the background color
 
-#define UPPERBACKGROUNDRED 0 // The amount of red in the upper background color
-#define UPPERBACKGROUNDGREEN 0 // The amount of green in the upper background color
-#define UPPERBACKGROUNDBLUE 0 // The amount of blue in the upper background color
+#define outerBACKGROUNDRED 0 // The amount of red in the outer background color
+#define outerBACKGROUNDGREEN 0 // The amount of green in the outer background color
+#define outerBACKGROUNDBLUE 0 // The amount of blue in the outer background color
