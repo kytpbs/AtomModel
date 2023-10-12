6e2827430846da2dff6bae7227c141f60af8862e
Kaya <95276965+kytpbs@users.noreply.github.com>
2023-10-12 23:38:00 +0300

    Move Constants around
    for better Readability

--- Constants.h
+++ Constants.h
@@ -2,9 +2,6 @@
 #define INNUMPIXELS 196 // The number of LEDs in the inner strip
 #define SMALLNUMPIXELS 116 // The number of LEDs in the small strip
 
-#define OUTDATA 13 // The pin that the outer strip is connected to
-#define INDATA 12 // The pin that the inner strip is connected to,
-#define SMALLDATA 11 // The pin that the small strip is connected to
 
 /* NEW DEFFINITONS */
 #define OUTDATA1 13 // The pin that the outer strip1 is connected to
@@ -14,10 +11,12 @@
 #define INDATA1 10 // The pin that the inner strip1 is connected to
 #define INDATA2 9 // The pin that the inner strip2 is connected to
 
+#define SMALLDATA 8 // The pin that the small strip is connected to
 
 #define INPIXELAMOUNT 8 // The number of pixels that are lit up on each strips in total (will change on pixel switch)
 #define OUTPIXELAMOUNT 10 // The number of pixels that are lit up on the outside strips in total (will change on pixel switch)
 #define SMALLPIXELAMOUNT 2 // The number of pixels that are lit up on the small strip (will NOT change on pixel switch)
+
 
 /*
  * The number of pixels between the two pixels including the pixel that are lit up. Should be devidble by both the outer and inner number of pixels.
@@ -31,20 +30,6 @@
 
 #define BRIGHTNESS 255 // The brightness of the LEDS should be a value from 0 - 255
 
-//Electron Colors of the small Strip should be values from (0 - 255):
-#define smallRED 255
-#define smallGREEN 0
-#define smallBLUE 180
-
-//Electron Colors of the inner Stshould be values from (0 - 255):
-#define innerRED 255
-#define innerGREEN 0
-#define innerBLUE 255
-
-// Electron Colors of the outer Strip should be values from (0 - 255):
-#define outerRED 0
-#define outerGREEN 0
-#define outerBLUE 255
 
 #define DELAYTIME 0 // The amount of time between each pixel movement in milliseconds.
 #define TOGETHERAMOUNT 2 // The amount of times the pixels need to be together to switch
@@ -55,6 +40,23 @@
 #define BLINKRED 255 // The amount of red in the blink
 #define BLINKGREEN 255 // The amount of green in the blink
 #define BLINKBLUE 255 // The amount of blue in the blink
+
+
+/* STRIP COLOR CONSTANTS */
+
+//Electron Color Constants
+#define smallRED 255   // Red color of the small Strip Electron should be values from (0 - 255)
+#define smallGREEN 0   // Green color of the small Strip Electron should be values from (0 - 255)
+#define smallBLUE 180  // Blue color of the small Strip Electron should be values from (0 - 255)
+
+#define innerRED 255   // Red color of the inner Strip Electron should be values from (0 - 255)
+#define innerGREEN 0   // Green color of the inner Strip Electron should be values from (0 - 255)
+#define innerBLUE 255  // Blue color of the inner Strip Electron should be values from (0 - 255)
+
+#define outerRED 0     // Red color of the outer Strip Electron should be values from (0 - 255)
+#define outerGREEN 0   // Green color of the outer Strip Electron should be values from (0 - 255)
+#define outerBLUE 255  // Blue color of the outer Strip Electron should be values from (0 - 255)
+
 
 //Background Color Constants
 #define smallBACKGROUNDRED 0 // The amount of red in the background color
