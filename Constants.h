154a046c65fd5515d7cd72b50274b02467a98f43
Kaya <95276965+kytpbs@users.noreply.github.com>
2023-10-16 09:25:25 +0300

    Update Constants.h

--- Constants.h
+++ Constants.h
@@ -1,9 +1,11 @@
 #define OUTNUMPIXELS 160 // The number of LEDs in the outer strip
 #define INNUMPIXELS 196 // The number of LEDs in the inner strip
-#define SMALLNUMPIXELS 116 // The number of LEDs in the small strip
+#define SMALLNUMPIXELS 115 // The number of LEDs in the small strip
 
 #define OUTDATA 13 // The pin that the outer strip is connected to
+#define OUTDATA2 10 // The pin that the outer strip is connected to
 #define INDATA 12 // The pin that the inner strip is connected to,
+#define INDATA2 9 // The pin that the inner strip is connected to,
 #define SMALLDATA 11 // The pin that the small strip is connected to
 
 #define INPIXELAMOUNT 8 // The number of pixels that are lit up on each strips in total (will change on pixel switch)
