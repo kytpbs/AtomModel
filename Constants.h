0fae079db03ff8d1c2d6b5340920effc0d164dae
Kaya <95276965+kytpbs@users.noreply.github.com>
2023-11-07 13:09:06 +0300

    add different pin layouts for each board

--- Constants.h
+++ Constants.h
@@ -4,12 +4,34 @@
 #define INNUMPIXELS 196 // The number of LEDs in the inner strip
 #define SMALLNUMPIXELS 115 // The number of LEDs in the small strip
 
+#ifdef ARDUINO_ARCH_ESP32 // only include if we are on an ESP
+
 #define OUTDATA 23 // The pin that the outer strip is connected to
 #define OUTDATA2 22 // The pin that the outer strip is connected to
 #define OUTDATA3 21 // The pin that the outer strip is connected to
 #define INDATA 19 // The pin that the inner strip is connected to
 #define INDATA2 18 // The pin that the inner strip is connected to
 #define SMALLDATA 17 // The pin that the small strip is connected to
+
+#elif defined(ARDUINO_ARCH_ESP8266) // only include if we are on an ESP
+
+#define OUTDATA 13 // The pin that the outer strip is connected to
+#define OUTDATA2 12 // The pin that the outer strip is connected to
+#define OUTDATA3 14 // The pin that the outer strip is connected to
+#define INDATA 5 // The pin that the inner strip is connected to
+#define INDATA2 4 // The pin that the inner strip is connected to
+#define SMALLDATA 15 // The pin that the small strip is connected to
+
+#else // We are on a non-ESP board
+
+#define OUTDATA 7 // The pin that the outer strip is connected to
+#define OUTDATA2 6 // The pin that the outer strip is connected to
+#define OUTDATA3 5 // The pin that the outer strip is connected to
+#define INDATA 4 // The pin that the inner strip is connected to
+#define INDATA2 3 // The pin that the inner strip is connected to
+#define SMALLDATA 2 // The pin that the small strip is connected to
+
+#endif
 
 #define INPIXELAMOUNT 8 // The number of pixels that are lit up on each strips in total (will change on pixel switch)
 #define OUTPIXELAMOUNT 10 // The number of pixels that are lit up on the outside strips in total (will change on pixel switch)
