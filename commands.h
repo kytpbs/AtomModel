90cffe2f5ee7c433f7abfdd51b5ed5379e4b8276
Kaya <95276965+kytpbs@users.noreply.github.com>
2023-11-29 11:22:28 +0300

    add echo command

--- commands.h
+++ commands.h
@@ -1,14 +1,24 @@
 #ifndef COMMANDS_H
 #define COMMANDS_H
 #include "serialCommandsSystem.h"
+#include "stringTools.h"
 #include <vector>
 
 void ping(CloudSerialSystem* cloudSerialSystem, std::vector<String>* argv) {
     cloudSerialSystem->print("pong");
 }
 
+void echo(CloudSerialSystem* cloudSerialSystem, std::vector<String>* argv) {
+    if (argv->size() == 0) {
+        cloudSerialSystem->print("No arguments provided!");
+        return;
+    }
+    cloudSerialSystem->print(joinString(argv));
+}
+
 void setupCommands(CloudSerialSystem* cloudSerialSystem) {
     cloudSerialSystem->addCommand("ping", ping);
+    cloudSerialSystem->addCommand("echo", echo);
 }
 
 #endif
